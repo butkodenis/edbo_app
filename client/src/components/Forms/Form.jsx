c1e890a5945139350f67f7bc397ee05cdf56fadd
Butko Denis <butko.dv@gmail.com>
2023-11-01 14:46:40 +0300

    оработка select

--- client/src/components/Forms/Form.jsx
+++ client/src/components/Forms/Form.jsx
@@ -5,10 +5,19 @@ import dictionary from '../dict';
 
 function Forms() {
   const { years, specialty, qualification, educationBase } = dictionary;
-  const [selectedJob, setSelectedJob] = useState('');
+  const [selectedYear, setSelectedYear] = useState('2023');
+  const [selectedSpecialty, setSelectedSpecialty] = useState('221');
+  const [selectedQualification, setSelectedQualification] = useState('1');
+  const [selectedEducationBase, setSelectedEducationBase] = useState('40');
+  const [selectedJob, setSelectedJob] = useState('saveAll');
+
   const handleClick = (event) => {
     event.preventDefault();
-    console.log('testing');
+    console.log('Selected year:', selectedYear);
+    console.log('Selected Specialty:', selectedSpecialty);
+    console.log('Selected Qualification:', selectedQualification);
+    console.log('Selected Education Base:', selectedEducationBase);
+    console.log('Selected Job:', selectedJob);
   };
 
   return (
@@ -16,13 +25,19 @@ function Forms() {
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
-            <Select name="yearSelect" label="Оберіть рік" values={years} />
+            <Select
+              name="yearSelect"
+              label="Оберіть рік"
+              values={years}
+              onChange={(event) => setSelectedYear(event.target.value)}
+            />
           </div>
           <div className="col-lg-6">
             <Select
               name="specialtySelect"
               label="Оберіть спеціальність"
               values={specialty}
+              onChange={(event) => setSelectedSpecialty(event.target.value)}
             />
           </div>
         </div>
@@ -32,6 +47,7 @@ function Forms() {
               name="qualificationySelect"
               label="Оберіть освітній рівень"
               values={qualification}
+              onChange={(event) => setSelectedQualification(event.target.value)}
             />
           </div>
           <div className="col-lg-6">
@@ -39,6 +55,7 @@ function Forms() {
               name="seducationBaseSelect"
               label="Оберіть основу вступу"
               values={educationBase}
+              onChange={(event) => setSelectedEducationBase(event.target.value)}
             />
           </div>
         </div>
--- client/src/components/Forms/Input/Select.jsx
+++ client/src/components/Forms/Input/Select.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-function Select({ name, values, label }) {
+function Select({ name, values, label, onChange }) {
   return (
     <>
       <label htmlFor={name} className="form-label">
         <strong>{label}</strong>
       </label>
-      <select id={name} className="form-select">
+      <select id={name} className="form-select" onChange={onChange}>
         {values.map((val) => (
           <option key={val.code} value={val.code}>
             {val.name}{' '}
