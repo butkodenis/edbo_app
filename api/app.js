6d5becc0ae7fc5783a7215670a46a0d6dd226800
Butko Denis <butko.dv@gmail.com>
2023-11-02 11:30:12 +0200

    включил заголовки CORS

--- api/app.js
+++ api/app.js
@@ -1,16 +1,36 @@
-const express = require("express");
-const logger = require("morgan");
-const { MongoClient } = require("mongodb");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get("/", async (req, res) => {
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/task/all', async (req, res) => {
   try {
-    const user = "messaage";
+    const user = 'messaage';
 
     res.json(user);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: "Ошибка на сервере" });
+    res.status(500).json({ error: 'Ошибка на сервере' });
+    return;
+  }
+});
+
+app.post('/task/all', async (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    res.json({ message: 'Данные успешно обработаны' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Ошибка на сервере' });
     return;
   }
 });
