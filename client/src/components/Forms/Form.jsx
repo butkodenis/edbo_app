1db1f2ff4a2368273673f0c5ba414f7aca5920ec
Butko Denis <butko.dv@gmail.com>
2023-11-10 11:03:27 +0200

    тест useForm

--- client/src/components/Forms/Form.jsx
+++ client/src/components/Forms/Form.jsx
@@ -1,40 +1,76 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
-import dictionary from '../dict';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Form = function () {
-  const { years, specialtys } = dictionary;
-  const renderSelectOptions = (items) =>
-    items.map((item) => (
-      <option key={item.code} value={item.code}>
-        {item.name}
-      </option>
-    ));
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <form>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg">
-            <label htmlFor="year" className="form-label">
-              <strong>выберити год</strong>
+    <div className="container mt-3">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="select1" className="form-label">
+            Выберите опцию 1:
+          </label>
+          <select id="select1" className="form-select" {...register('select1')}>
+            <option value="">Выберите...</option>
+            <option value="option1">Опция 1</option>
+            <option value="option2">Опция 2</option>
+            <option value="option3">Опция 3</option>
+          </select>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="select2" className="form-label">
+            Выберите опцию 2:
+          </label>
+          <select id="select2" className="form-select" {...register('select2')}>
+            <option value="">Выберите...</option>
+            <option value="option4">Опция 4</option>
+            <option value="option5">Опция 5</option>
+            <option value="option6">Опция 6</option>
+          </select>
+        </div>
+
+        <div className="mb-3">
+          <div className="form-check">
+            <input
+              type="radio"
+              id="radio1"
+              className="form-check-input"
+              value="radioOption1"
+              {...register('radioOption')}
+            />
+            <label htmlFor="radio1" className="form-check-label">
+              Радио опция 1
             </label>
-            <select id="year" className="form-select">
-              {renderSelectOptions(years)}
-            </select>
-          </div>
-          <div className="col-lg">
-            <label htmlFor="specialty" className="form-label">
-              <strong>выберити specialty</strong>
-            </label>
-            <select id="specialty" className="form-select">
-              {renderSelectOptions(specialtys)}
-            </select>
           </div>
         </div>
-      </div>
-      <button type="submit">создать задачу</button>
-    </form>
+
+        <div className="mb-3">
+          <div className="form-check">
+            <input
+              type="radio"
+              id="radio2"
+              className="form-check-input"
+              value="radioOption2"
+              {...register('radioOption')}
+            />
+            <label htmlFor="radio2" className="form-check-label">
+              Радио опция 2
+            </label>
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Отправить
+        </button>
+      </form>
+    </div>
   );
 };
 
