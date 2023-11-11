270ebefebdbf85b89baad0f581ccf9fe77ec5b18
butkodenis <Butko.dv@gmail.com>
2023-11-11 22:14:42 +0200

    влил изменения в форме

---
+++ client/src/components/Forms/Input/Radio.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+function Radio({ name, value, label, register }) {
+  return (
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="radio"
+        name={name}
+        value={value}
+        id={value}
+        {...register(name, { text: label })}
+        defaultChecked={value === 'saveAll'}
+      />
+      <label htmlFor={name} className="form-check-label">
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default Radio;
