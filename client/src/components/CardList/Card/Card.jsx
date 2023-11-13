4c7743475a1ea6dbb8674878de2361f062c32af8
Butko Denis <butko.dv@gmail.com>
2023-11-13 12:52:41 +0200

    импорт изменений из  card

---
+++ client/src/components/CardList/Card/Card.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import axios from 'axios';
+
+function Card({ card, fetchData }) {
+  const {
+    _id,
+    year,
+    specialty,
+    specialtyText,
+    qualificationText,
+    educationBaseText,
+    taskText,
+  } = card;
+  const handleDelete = async () => {
+    try {
+      // Отправка запроса на удаление с использованием Axios
+      await axios.delete(`http://localhost:4040/task/delete`, {
+        data: { id: _id },
+      });
+
+      // Дополнительные действия после успешного удаления, если нужно
+      console.log(`Task with ID ${_id} deleted successfully.`);
+      fetchData();
+    } catch (error) {
+      // Обработка ошибок при удалении
+      console.error('Error deleting task:', error.message);
+    }
+  };
+
+  return (
+    <div className="col-lg-4 col-md-4" key={_id}>
+      <div className="card  text-bg-light mb-4">
+        <div className="card-header">
+          <button
+            type="button"
+            className="btn-close"
+            aria-label="Close"
+            onClick={handleDelete}
+          />
+        </div>
+
+        <div className="card-body">
+          <p className="">
+            {taskText} {year}
+          </p>
+          <p className="">ID: {_id}</p>
+          <p className="">
+            <strong>
+              {specialty} {specialtyText}
+            </strong>
+          </p>
+
+          <p className="">
+            ОР -<strong> {qualificationText}</strong>{' '}
+          </p>
+          <p className="">ОВ - {educationBaseText}</p>
+        </div>
+        <div className="card-footer text-body-secondary">
+          <button type="button" className="btn btn-outline-primary btn-sm">
+            Імпорт
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ client/src/components/CardList/CardList.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Card from './Card/Card';
+
+function CardList({ cardsData, fetchData }) {
+  return (
+    <div className="card-list mt-4 mb-4">
+      <div className="container">
+        <div className="row">
+          {cardsData.map((card) => (
+            <Card key={card._id} card={card} fetchData={fetchData} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardList;
