a940e79d09967b1da184c2018912299ac1808ada
Butko Denis <butko.dv@gmail.com>
2023-12-04 15:45:39 +0200

    уствновил router

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Form from '../components/Forms/Form';
+import CardList from '../components/CardList/CardList';
+
+function Homepage() {
+  const [cardsData, setCardsData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:4040/task/all');
+      setCardsData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(cardsData);
+  }, []);
+
+  return (
+    <div className="container-fluid ">
+      <div className="row">
+        <div className="col-12">
+          <Form fetchData={fetchData} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <CardList cardsData={cardsData} fetchData={fetchData} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
