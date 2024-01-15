f85b423874717cbe67b3110fb99b7fe3f92186f5
Butko Denis <butko.dv@gmail.com>
2024-01-15 09:07:39 +0200

    добавил сортировку карточек

--- client/src/components/CardList/CardList.jsx
+++ client/src/components/CardList/CardList.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import Card from './Card/Card';
 
 function CardList({ cardsData, fetchData }) {
+  // Сортируем массив cardsData по времени создания (timeCreation)
+  const sortedCardsData = cardsData.sort(
+    (a, b) => new Date(a.timeCreation) - new Date(b.timeCreation),
+  );
   return (
     <div className="card-list mt-4 mb-4">
       <div className="container-fluid">
